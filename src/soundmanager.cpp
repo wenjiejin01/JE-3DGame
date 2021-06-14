9f8b622aea60f8ee24a71335ee22c6a3de4b2b0a
Wenjie_Jin <32678710+daw2-Jin18@users.noreply.github.com>
2021-06-14 10:05:07 +0200

    git error

---
+++ src/soundmanager.cpp
@@ -0,0 +1,55 @@
+#include "soundmanager.h"
+
+SoundManager::SoundManager() {
+	//Inicializamos BASS  (id_del_device, muestras por segundo, ...)
+	BASS_Init(-1, 44100, BASS_DEVICE_DEFAULT, 0, NULL);
+}
+
+void SoundManager::playSound(const std::string& name, bool loop)
+{
+	std::string sound = "data/sounds/" + name + ".mp3";
+
+	auto it = samples.find(name);
+	if (it != samples.end())
+	{
+		// stop if the sound is being played //
+		StopSound(name);
+
+		HCHANNEL hSampleChannel;
+		if (channels[name] == -1)
+		{
+			hSampleChannel = BASS_SampleGetChannel(samples[name], false);
+			channels[name] = hSampleChannel;
+		}
+		else
+		{
+			hSampleChannel = channels[name];
+		}
+		BASS_ChannelPlay(hSampleChannel, loop);
+		return;
+	}
+
+	HSAMPLE hSample = BASS_SampleLoad(false, sound.c_str(), 0L, 0, 1, loop ? BASS_SAMPLE_LOOP : 0);
+	HCHANNEL hSampleChannel = BASS_SampleGetChannel(hSample, false);
+
+	samples[name] = hSample;
+	channels[name] = hSampleChannel;
+
+	BASS_ChannelPlay(hSampleChannel, loop);
+}
+
+void SoundManager::StopSound(const std::string& name)
+{
+	//std::cout << "stopping sound.. " << name << std::endl;
+
+	auto it = samples.find(name);
+	if (it != samples.end())
+	{
+		HCHANNEL hSampleChannel = channels[name];
+		BASS_ChannelStop(hSampleChannel);
+		channels[name] = -1;
+		return;
+	}
+
+	std::cout << "could not find sample.. " << name << std::endl;
+}
---
+++ src/soundmanager.h
@@ -0,0 +1,34 @@
+#ifndef SOUNDMANAGER_H
+#define SOUNDMANAGER_H
+
+#include "utils.h"
+#include <iostream>
+#include <vector>
+#include "framework.h"
+#include "game.h"
+#include "extra/bass.h"
+
+
+class SoundManager
+{
+public:
+	SoundManager();
+	~SoundManager();
+
+	static SoundManager* instance;
+
+	static SoundManager* getInstance() {
+		if (instance == NULL) instance = new SoundManager();
+		return instance;
+	}
+
+	void playSound(const std::string& name, bool loop);
+	void StopSound(const std::string& name);
+	void SetVolume(const std::string& name, float value);
+
+	std::map<std::string, HCHANNEL> channels;
+	std::map<std::string, HSAMPLE> samples;
+};
+
+#endif	
+
